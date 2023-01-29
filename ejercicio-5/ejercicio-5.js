2b4c2f60ed94b14652920b1d84c21c9f5cfa6207
Santiago Montoya <smonto67@gmail.com>
2023-01-29 14:14:26 -0500

    ejercicio-5 ok

---
+++ ejercicio-5/ejercicio-5.js
@@ -0,0 +1,9 @@
+const alturaCm = 168;
+const alturaM = 1.68;
+const pesoKg = 75.2;
+const alturaRarriba = Math.ceil(alturaM);
+console.log("La altura redondeada hacia arriba es",alturaRarriba, "metros");
+const pesoRabajo = Math.round(pesoKg);
+console.log("El peso redondeado hacia abajo es",pesoRabajo, "Kg");
+maxValor = (Number.MAX_SAFE_INTEGER +1) === Number.MAX_VALUE;
+console.log("El mayor numero de javascript es igual al mayor numero de javascript +1?", maxValor);
