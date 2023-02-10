5174cc7ef1819e0ddc204c8ebac0a660eaea6e6e
Santiago Montoya <smonto67@gmail.com>
2023-02-09 19:09:43 -0500

    ejercicio sets OK

---
+++ ejercicio-7/conjuntos.js
@@ -0,0 +1,7 @@
+const miFamilia = ["Guillermo","Rosalba","Juan David","Manuela","Santiago"];
+const conjunto = new Set(miFamilia);
+console.log(conjunto);
+conjunto.add("Santiago");
+console.log(conjunto);
+conjunto.add("Javascript");
+console.log(conjunto);
